786031be6afdf8b961684f4f1dea55739aba7749
Enrique Palacios <enrique.palacios@struck.com>
2020-05-24 00:39:53 -0600

    Remvove unnecessary imports

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -6,10 +6,6 @@ import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import Checkbox from "@material-ui/core/Checkbox";
 import { makeStyles } from "@material-ui/core/styles";
-import ExpansionPanel from "@material-ui/core/ExpansionPanel";
-import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
-import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import MuiAlert from "@material-ui/lab/Alert";
 
 const useStyles = makeStyles({
