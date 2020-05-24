6a4119d5853fb389c7a137e51533a659273ede7a
Enrique Palacios <enrique.palacios@struck.com>
2020-05-24 14:28:19 -0600

    Swap among exam versions

--- src/components/Simulator.jsx
+++ src/components/Simulator.jsx
@@ -2,6 +2,11 @@ import React, { useState } from "react";
 import Stepper from "./Stepper";
 import Question from "./Question";
 import Typography from "@material-ui/core/Typography";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import { makeStyles } from "@material-ui/core/styles";
 
 const resetQuestions = (questions) => {
   return questions.map((q) => {
@@ -10,10 +15,24 @@ const resetQuestions = (questions) => {
   });
 };
 
+const useStyles = makeStyles((theme) => ({
+  examInfo: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingBottom: "1%",
+  },
+  versionSelector: {
+    marginLeft: "1%",
+  },
+}));
+
 const Simulator = ({ exam }) => {
+  const classes = useStyles();
   const [activeStep, setActiveStep] = useState(0);
   const [score, setScore] = useState(false);
   const [questions, setQuestions] = useState(resetQuestions(exam.versions[0]));
+  const [versionIndex, setVersionIndex] = React.useState(0);
 
   const handleNext = () => {
     const evaluated = questions.map((q, i) => {
@@ -46,6 +65,12 @@ const Simulator = ({ exam }) => {
     setQuestions(newQuestions);
   };
 
+  const setAnotherExam = (event) => {
+    const index = event.target.value;
+    setVersionIndex(index);
+    setQuestions(resetQuestions(exam.versions[index]));
+  };
+
   if (score) {
     const correct = questions.filter((q) => q.evaluation);
     const incorrect = questions.filter((q) => !q.evaluation);
@@ -69,6 +94,26 @@ const Simulator = ({ exam }) => {
   } else {
     return (
       <>
+        <div className={classes.examInfo}>
+          <Typography variant="h5" gutterBottom>
+            {exam.title}
+          </Typography>
+          <FormControl className={classes.versionSelector}>
+            <InputLabel id="demo-simple-select-label">Version</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={versionIndex}
+              onChange={setAnotherExam}
+            >
+              {exam.versions.map((v, i) => (
+                <MenuItem key={i} value={i}>
+                  {i + 1}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
         <Stepper
           totalQuestions={exam.totalQuestions}
           activeStep={activeStep}
