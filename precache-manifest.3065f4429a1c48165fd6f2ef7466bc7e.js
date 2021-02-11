cef6c9868b71fdf50b86f81554d264c6f3447a95
Enrique Palacios <enrique.palacios@struck.com>
2021-02-11 12:16:07 -0600

    Updates

---
+++ precache-manifest.3065f4429a1c48165fd6f2ef7466bc7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5306c95136ac9df1eab4b79af2a4e24e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d76e3fb2403f210e2ed6",
+    "url": "/static/css/main.1b5f9337.chunk.css"
+  },
+  {
+    "revision": "c9c66c23970db2324dc2",
+    "url": "/static/js/2.beb57534.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.beb57534.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d76e3fb2403f210e2ed6",
+    "url": "/static/js/main.fd31258b.chunk.js"
+  },
+  {
+    "revision": "67d46388c4c89b30768b",
+    "url": "/static/js/runtime-main.f0c0c307.js"
+  }
+]);
